6dd7270971d829bd6c6bce5a3fa88a1b9507d246
Nicol <beltrannicole384@gmail.com>
2023-09-27 10:10:45 -0500

    Add Controllers: Service_Type, Service_Category, Service_Type_Category

---
+++ app/Http/Controllers/Service_TypeController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Service_Type;
+
+class Service_TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $service_types = Service_Type::all();
+        return view('service_type.index', compact('service_types'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('service_type.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Service_Type::create([
+            'name' => $request->get('name')
+        ]);
+
+        return redirect('service_type');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $service_type = Service_Type::find($id);
+
+        return view('service_type.edit', compact('service_type'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $service_type =Service_Type::find($id);
+        $service_type->name = $request->input('name');
+
+        $service_type->update();
+
+        return redirect('service_type');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $service_type = Service_Type::find($id);
+        $service_type->delete();
+
+        return redirect('service_type');
+    }
+}
---
+++ app/Http/Controllers/Service_Type_CategoryController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Service_Type_CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Ejemplo de obtener los tipos de servicio relacionados con una categoría específica
+$category = Service_Category::find(1);
+$serviceTypes = $category->serviceTypeCategory->map(function ($item) {
+    return $item->serviceType;
+});
+
+// Ejemplo de obtener las categorías de servicio relacionadas con un tipo de servicio específico
+$type = Service_Type::find(1);
+$serviceCategories = $type->serviceTypeCategory->map(function ($item) {
+    return $item->serviceCategory;
+});
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Obtener todas las categorías de un tipo de servicio
+$serviceType = Service_Types::find(1);
+$categories = $serviceType->categories;
+
+// Obtener todos los tipos de servicio de una categoría
+$category = Service_Categories::find(1);
+$types = $category->types;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
