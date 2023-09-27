f980dbed931c89c0769c25492968eeb6612c8720
Alexandra Merchan <geralex01@gmail.com>
2023-09-27 11:57:41 -0500

    ✨ Add service_type CRUD

--- resources/views/service_type/create.blade.php
+++ resources/views/service_type/create.blade.php
@@ -1,21 +1,20 @@
 @extends('layouts.app')
-@section('title','Municipios')
+@section('title','Tipo de servicios')
 
 @section('content')
 
-<h1 class="text-center">Crear Municipios</h1>
+<h1 class="text-center">Crear Tipo de Servicios</h1>
 
 <div class="mx-5 px-5">
-    <form action="{{Route('municipality.store')}}" method="POST">
+    <form action="{{Route('service_type.store')}}" method="POST">
         @csrf
         <div class="form-group">
-            <label for="name">NOMBRE</label>
-            <input type="text" class="form-control " id="name" name="name" placeholder="Ingrese el nombre del municipio" require>
+            <label for="name">TIPO DE SERVICIO</label>
+            <input type="text" class="form-control " id="name" name="name" placeholder="Ingrese el tipo de servicio" require>
         </div>
 
-        <button type="submit" class="btn btn-success">Crear</button>
-        <a href="./" class="btn btn-danger">Cancelar</a>
-
+        <button type="submit" class="btn btn-success"><i class="bi bi-plus-lg"></i> Crear</button>
+        <a href="{{ route('service_type.index') }}" class="btn btn-danger"><i class="bi bi-x-lg"></i> Cancelar</a>
     </form>
 </div>
 @stop
--- resources/views/service_type/edit.blade.php
+++ resources/views/service_type/edit.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
-@section('title','Municipios')
+@section('title','Tipo de Servicio')
 
 @section('content')
 
-<h1 class="text-center">Editar Municipios</h1>
+<h1 class="text-center">Editar Tipo de Servicio</h1>
 
 <div class="mx-5 px-5">
-    <form action="{{Route('municipality.update', $municipality->id)}}" method="POST">
+    <form action="{{Route('service_type.update', $service_type->id)}}" method="POST">
         @csrf
         @method('PUT')
         
         <div class="form-group">
-            <label for="name">NOMBRE</label>
-            <input type="text" class="form-control" value="{{ $municipality->name }}" id="name" name="name" placeholder="Ingrese el nombre del municipio" require>
+            <label for="name">TIPO DE SERVICIO</label>
+            <input type="text" class="form-control" value="{{ $service_type->name }}" id="name" name="name" placeholder="Ingrese el tipo de servicio" require>
         </div>
 
-        <button type="submit" class="btn btn-success">Editar</button>
-        <a href="./" class="btn btn-danger">Cancelar</a>
+        <button type="submit" class="btn btn-success"><i class="bi bi-pencil-square"></i> Editar</button>
+        <a href="{{ route('service_type.index') }}" class="btn btn-danger"><i class="bi bi-x-lg"></i> Cancelar</a>
 
     </form>
 </div>
