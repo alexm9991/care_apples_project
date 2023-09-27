1741ce9630ad9a6bfb7304fadb58ef8d74c2e3f9
Alexandra Merchan <geralex01@gmail.com>
2023-09-27 12:01:40 -0500

    🚚  Add routes Service_Type_Category

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MunicipalityController;
 use App\Http\Controllers\AppleController;
 use App\Http\Controllers\Service_TypeController;
 use App\Http\Controllers\Service_CategoryController;
+use App\Http\Controllers\Service_Type_CategoryController;
 
 
 /*
@@ -29,5 +30,7 @@ Route::resource('municipality', MunicipalityController::class);
 Route::resource('apple', AppleController::class);
 Route::resource('service_type', Service_TypeController::class);
 Route::resource('service_category', Service_CategoryController::class);
+Route::resource('service_type_category', Service_Type_CategoryController::class);
+
 
 
