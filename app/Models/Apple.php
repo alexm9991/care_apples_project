4279cfd50f89a740c8d38914a0a3aacfb6f52382
Alexandra Merchan <geralex01@gmail.com>
2023-09-27 10:07:54 -0500

    ✨ Update apple controller and model

--- app/Models/Apple.php
+++ app/Models/Apple.php
@@ -8,10 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Apple extends Model
 {
     use HasFactory;
-    protected $fillable=['code','name','location','address','coordinates','municipalities_id'];
+    protected $fillable=[
+        'code','name','location','address','latitude',
+        'length','municipalities_id'
+    ];
+
     protected $table='apples';
 
     public function municipalities(){
-        return $this->hasMany(Municipality::class,'municipalities_id');
+        return $this->hasMany('App\Models\Municipality', 'id');
     }
+
 }
